df62754cdbb31c0223280c60114e9942dd5bfe2b
Wingston Sharon <wingston.sharon@gmail.com>
2015-09-15 21:43:38 +0400

    initial commit

---
+++ index.android.js
@@ -0,0 +1,116 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
+var REQUEST_URL = 'http://wingztv.com/public_api/random/';
+// var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+var AwesomeProject = React.createClass({
+  getInitialState: function() { // only for returning values
+    return {
+      movies: null,
+    };
+  },
+  componentDidMount: function() { // called once
+    this.fetchData();
+  },
+  fetchData: function() {
+    console.log(REQUEST_URL);
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.result,
+        });
+      })
+      .done();
+      // console.log(this.getState());
+  },
+  render: function() {
+    console.log('###'+this.state.movies)
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+    var movie = this.state.movies[0];
+    console.log(movie)
+    return this.renderMovie(movie);
+  },
+
+  renderLoadingView: function() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  },
+
+  renderMovie: function(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.news_url}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.news_title}</Text>
+          <Text style={styles.year}>{movie.news_date}</Text>
+        </View>
+      </View>
+    );
+  },
+
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
